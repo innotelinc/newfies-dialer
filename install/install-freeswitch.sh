bdaac1dca8738e6b72a5a37b66022313a3616e07
innotelinc <68697450+innotelinc@users.noreply.github.com>
2023-10-16 02:01:00 -0400

    Update install-freeswitch.sh

--- install/install-freeswitch.sh
+++ install/install-freeswitch.sh
@@ -15,7 +15,7 @@
 
 #
 # To download and run the script on your server :
-# cd /usr/src/ ; rm install-freeswitch.sh ; wget --no-check-certificate https://raw.github.com/newfies-dialer/newfies-dialer/develop/install/install-freeswitch.sh ; chmod +x install-freeswitch.sh ; ./install-freeswitch.sh
+# cd /usr/src/ ; rm install-freeswitch.sh ; wget --no-check-certificate https://raw.githubusercontent.com/innotelinc/newfies-dialer/develop/install/install-freeswitch.sh ; chmod +x install-freeswitch.sh ; ./install-freeswitch.sh
 #
 
 # Set branch to install develop / default: master

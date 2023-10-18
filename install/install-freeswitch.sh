bed56d8d3840404ffb9f1f563c9bcf9068adcf75
innotelinc <68697450+innotelinc@users.noreply.github.com>
2023-10-18 15:31:37 -0400

    Update install-freeswitch.sh

--- install/install-freeswitch.sh
+++ install/install-freeswitch.sh
@@ -111,13 +111,15 @@ func_install_fs_sources() {
     update-alternatives --install /usr/bin/python python /usr/bin/python2 1
     update-alternatives --install /usr/bin/python python /usr/bin/python2 1
     curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2
-    wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
-    #wget https://cmake.org/files/v3.7/cmake-3.27.7.tar.gz #Ubuntu 22
-    tar -zxf cmake-3.7.2.tar.gz
-    cd cmake-3.7.2
-    ./bootstrap.sh && ./configure && make && make install
-    ldconfig
-    cd ..
+    #Install CMake 3.7.2 for Debian 8 & Ubuntu 20
+    #wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
+    #Install for Ubuntu 22
+    ##wget https://cmake.org/files/v3.7/cmake-3.27.7.tar.gz
+    #tar -zxf cmake-3.7.2.tar.gz
+    #cd cmake-3.7.2
+    #./bootstrap.sh && ./configure && make && make install
+    #ldconfig
+    #cd ..
     git clone https://github.com/innotelinc/spandsp.git
     cd spandsp
     ./bootstrap.sh && ./configure && make && make install

3131445db75ec493eb800df4403501ec291799f2
innotelinc <68697450+innotelinc@users.noreply.github.com>
2023-10-19 19:03:37 -0400

    Update install-freeswitch.sh

--- install/install-freeswitch.sh
+++ install/install-freeswitch.sh
@@ -111,16 +111,16 @@ func_install_fs_sources() {
     #apt -y install git yasm nasm pkg-config libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libswscale-dev libswresample-dev libavfilter-dev libopus-dev libshout3-dev libmpg123-dev curl lua-sec postgresql-contrib postgresql apt-transport-https uuid uuid-dev sudo python3-dev libsqlite3-dev libpcre3 libpcre3-dev libedit-dev libsndfile-dev libmp3lame-dev libldns-dev php-dev uuid uuid-dev python3-pip libtiff-dev pipx
     #update-alternatives --install /usr/bin/python python /usr/bin/python2 1
     #update-alternatives --install /usr/bin/python python /usr/bin/python2 1
-    #curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2
+    curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python
     #Install CMake 3.7.2 for Debian 8 & Ubuntu 20
-    #wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
+    wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
     #Install for Ubuntu 22
     ##wget https://cmake.org/files/v3.7/cmake-3.27.7.tar.gz
-    #tar -zxf cmake-3.7.2.tar.gz
-    #cd cmake-3.7.2
-    #./bootstrap.sh && ./configure && make && make install
-    #ldconfig
-    #cd ..
+    tar -zxf cmake-3.7.2.tar.gz
+    cd cmake-3.7.2
+    ./bootstrap.sh && ./configure && make && make install
+    ldconfig
+    cd ..
     git clone https://github.com/innotelinc/spandsp.git
     cd spandsp
     ./bootstrap.sh && ./configure && make && make install

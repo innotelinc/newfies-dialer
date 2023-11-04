a470a410bfd29c8beddd7df6ade7ee0320f656f1
innotelinc <68697450+innotelinc@users.noreply.github.com>
2023-11-04 19:37:56 -0400

    Update newfies-dialer-functions.sh

--- install/newfies-dialer-functions.sh
+++ install/newfies-dialer-functions.sh
@@ -181,8 +181,9 @@ func_install_dependencies(){
             apt-get -y install python-software-properties
             apt-get -y install python-setuptools python-dev build-essential
             apt-get -y install nginx supervisor
-            apt-get -y install git-core mercurial gawk cmake
-            apt-get -y install python-pip
+            apt-get -y install git mercurial gawk
+            # cmake
+            apt-get -y install python3-pip
             # for audiofile convertion
             apt-get -y install libsox-fmt-mp3 libsox-fmt-all mpg321
             #repeat flite install in case FS is on a different server

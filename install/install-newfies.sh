fd1c18a2dda4ccd255e20f520b615f9de9fb5dd6
innotelinc <68697450+innotelinc@users.noreply.github.com>
2023-11-04 18:03:42 -0400

    Update install-newfies.sh

--- install/install-newfies.sh
+++ install/install-newfies.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
-#
-# Newfies-Dialer License
-# http://www.newfies-dialer.org
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# Copyright (C) 2011-2015 Star2Billing S.L.
-#
-# The primary maintainer of this project is
-# Arezqui Belaid <info@star2billing.com>
-#
-
-#
-# To download and run the script on your server :
-#
-# >> Install with Master script :
-# cd /usr/src/ ; rm install-newfies.sh ; wget --no-check-certificate https://raw.github.com/newfies-dialer/newfies-dialer/master/install/install-newfies.sh ; chmod +x install-newfies.sh ; ./install-newfies.sh
-#
-# >> Install with develop script :
-# cd /usr/src/ ; rm install-newfies.sh ; wget --no-check-certificate https://raw.github.com/newfies-dialer/newfies-dialer/develop/install/install-newfies.sh ; chmod +x install-newfies.sh ; ./install-newfies.sh
-#
 
 # Set branch to install develop / default: master
 if [ -z "${BRANCH}" ]; then
@@ -29,8 +6,11 @@ if [ -z "${BRANCH}" ]; then
 fi
 
 #Get Scripts dependencies
+cd /usr/src/
+rm newfies-dialer-functions.sh
+wget --no-check-certificate https://raw.githubusercontent.com/innotelinc/newfies-dialer/develop/install/newfies-dialer-functions.sh -O newfies-dialer-functions.sh
 #Include cdr-stats install functions
-source $PWD/install/newfies-dialer-functions.sh
+source newfies-dialer-functions.sh
 
 
 #Menu Section for Script
